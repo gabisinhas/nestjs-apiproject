c6c5124214dbf1405d4c9292fe065861200b35e1
Gabriela dos Santos <gabisinhas@yahoo.com.br>
2025-05-02 10:06:02 -0300

    first commit.

---
+++ src/user/user.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Post, Body } from "@nestjs/common";
+
+@Controller('users')
+export class UserController {
+
+    @Post()
+    async create(@Body() body) {
+
+    return {body};
+
+    }
+}
